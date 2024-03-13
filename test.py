fc1769109644bda9724790f0e13169f0f624b431
unseeyou <siqi.chen111@gmail.com>
2024-03-13 19:33:35 +1100

    redirect to homepage

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def default():
+    return redirect('/home')
+
+
+@app.route('/home')
+def homepage():
     return render_template('home.html')
 
 
