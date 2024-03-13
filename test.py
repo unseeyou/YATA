e36a974eb68d5d6c34b9a90a56fdbe9f088eb928
unseeyou <siqi.chen111@gmail.com>
2024-03-13 21:21:50 +1100

    trying to implement login authentication

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request
+from requests_oauthlib import OAuth2Session
 import requests
 
 APP_ID = "01hrnv0jbne4zr0k11x45zdpcs"
@@ -18,8 +19,20 @@ def homepage():
 @app.route('/auth')
 def auth_page():
     querystring = request.args
-    print(querystring.get('authToken', default=None, type=str))
+    print(querystring.get('code', default=None, type=str))
     return redirect('/home')
+
+
+@app.route('/login')
+def login_page():
+    url = "https://auth.sbhs.net.au/authorize"
+    api = OAuth2Session(client_id=APP_ID, redirect_uri="https://yata.onrender.com/auth", scope="all-ro", pkce="plain", )
+    auth_url, state = api.authorization_url(url)
+    print(state)
+    print(auth_url)
+    # token = api.fetch_token(token_url="https://auth.sbhs.net.au/token")
+    return str(auth_url)
+# https://auth.sbhs.net.au/authorize?response_type=code&client_id=01hrnv0jbne4zr0k11x45zdpcs&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth&scope=all-ro&state=rnSviYURZiQGs6A2plL5ii2gg2I3Eb
 
 
 if __name__ == "__main__":
